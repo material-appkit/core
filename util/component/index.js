f03acfaaa3177eb83d7c0ca1e7d93dc68bfcd6e0
Allan Hart <allan.w.hart@gmail.com>
2019-01-13 11:30:46 +0800

    Added component utils

---
+++ util/component/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "recursiveMap", {
+  enumerable: true,
+  get: function get() {
+    return _component.recursiveMap;
+  },
+});
+
+Object.defineProperty(exports, "decorateErrors", {
+  enumerable: true,
+  get: function get() {
+    return _component.decorateErrors;
+  },
+});
+
+const _component = _interopRequireDefault(require("./component"));
