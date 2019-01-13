2ab12c43e3cd72bce56f734eb889317577c94e65
Allan Hart <allan.w.hart@gmail.com>
2019-01-13 08:57:11 +0800

    Added initial package definition

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
+
+Object.defineProperty(exports, "printMsg", {
+  enumerable: true,
+  get: function printMsg() {
+    return () => { console.log('Hello AppKit!'); }
+  }
+});
