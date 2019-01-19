6fb9e6e3e046ffa18e05168e48b14025f65e15e4
Allan Hart <allan.w.hart@gmail.com>
2019-01-19 12:21:07 +0800

    Added form utils

---
+++ util/form/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "formToObject", {
+  enumerable: true,
+  get: function get() {
+    return _form.formToObject;
+  },
+});
+
+const _form = _interopRequireDefault(require("./form"));
