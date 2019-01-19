4cb1ef5ea9d5d18caf29f6eed56d43c2f0922ffc
Allan Hart <allan.w.hart@gmail.com>
2019-01-19 12:06:35 +0800

    Added path utils

---
+++ util/path/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "include", {
+  enumerable: true,
+  get: function get() {
+    return _path.include;
+  },
+});
+
+const _path = _interopRequireDefault(require("./path"));
---
+++ util/path/path.js
@@ -0,0 +1,27 @@
+const _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+export function include(base, routes) {
+  const mappedRoutes = {
+    toString: function toString() {
+      return base;
+    },
+  };
+
+  Object.keys(routes).forEach(function (route) {
+    const url = routes[route];
+
+    if (typeof url === "function" && route === "toString") {
+      mappedRoutes.toString = function () {
+        return base + routes.toString();
+      };
+    } else if ((typeof url === "undefined" ? "undefined" : _typeof(url)) === "object") {
+      // nested include - prefix all sub-routes with base
+      mappedRoutes[route] = include(base, url);
+    } else {
+      // route - prefix with base and replace duplicate //
+      mappedRoutes[route] = url.indexOf("/") === 0 ? url : [base, url].join("/").replace("//", "/");
+    }
+  });
+
+  return mappedRoutes;
+}
