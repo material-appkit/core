fa72eea815fecc674e1ca16195adad216ebb989a
Allan Hart <allan.w.hart@gmail.com>
2019-01-19 12:10:33 +0800

    Added map utils

---
+++ util/map/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "firstObject", {
+  enumerable: true,
+  get: function get() {
+    return _path.firstObject;
+  },
+});
+
+Object.defineProperty(exports, "indexOfKey", {
+  enumerable: true,
+  get: function get() {
+    return _path.indexOfKey;
+  },
+});
+
+const _map = _interopRequireDefault(require("./map"));
