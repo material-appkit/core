a2375d53d7939576c679ffd5ee1dff9960487f85
Allan Hart <allan.w.hart@gmail.com>
2019-01-19 12:28:31 +0800

    Added VirtualizedList component

---
+++ components/VirtualizedList.js
@@ -0,0 +1,99 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _mobxReact = require('mobx-react');
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactInfiniteScroller = require('react-infinite-scroller');
+
+var _reactInfiniteScroller2 = _interopRequireDefault(_reactInfiniteScroller);
+
+var _CircularProgress = require('@material-ui/core/CircularProgress');
+
+var _CircularProgress2 = _interopRequireDefault(_CircularProgress);
+
+var _List = require('@material-ui/core/List');
+
+var _List2 = _interopRequireDefault(_List);
+
+var _ListItem = require('@material-ui/core/ListItem');
+
+var _ListItem2 = _interopRequireDefault(_ListItem);
+
+var _withStyles = require('@material-ui/core/styles/withStyles');
+
+var _withStyles2 = _interopRequireDefault(_withStyles);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var styles = function styles(theme) {
+  return {
+    root: {
+      width: '100%',
+      backgroundColor: theme.palette.background.paper
+    },
+
+    loadProgressListItem: {
+      justifyContent: 'center'
+    }
+  };
+};
+
+// @material-ui/core components
+
+
+var VirtualizedList = function VirtualizedList(props) {
+  var classes = props.classes;
+
+
+  var loadMoreProgressIndicator = _react2.default.createElement(
+    _ListItem2.default,
+    { className: props.classes.loadProgressListItem, key: 'loadMoreProgressIndicator' },
+    _react2.default.createElement(_CircularProgress2.default, { color: 'primary', size: 30, thickness: 5 })
+  );
+
+  return _react2.default.createElement(
+    _List2.default,
+    { className: classes.root },
+    props.store && props.store.items ? _react2.default.createElement(
+      _reactInfiniteScroller2.default,
+      {
+        initialLoad: false,
+        pageStart: 1,
+        loadMore: function loadMore(page) {
+          props.store.loadMore(page);
+        },
+        hasMore: !props.store.isLoaded,
+        loader: loadMoreProgressIndicator
+      },
+      props.store.items.map(function (item) {
+        return _react2.default.createElement(props.componentForItem, {
+          key: item.id,
+          item: item
+        });
+      })
+    ) : _react2.default.createElement(
+      _react2.default.Fragment,
+      null,
+      loadMoreProgressIndicator
+    )
+  );
+};
+
+VirtualizedList.propTypes = {
+  classes: _propTypes2.default.object,
+  componentForItem: _propTypes2.default.func.isRequired,
+  store: _propTypes2.default.object
+};
+
+exports.default = (0, _withStyles2.default)(styles)((0, _mobxReact.observer)(VirtualizedList));
---
+++ src/components/VirtualizedList.js
@@ -0,0 +1,67 @@
+import { observer } from 'mobx-react';
+
+import PropTypes from 'prop-types';
+import React from "react";
+import InfiniteScroll from 'react-infinite-scroller';
+
+import CircularProgress from '@material-ui/core/CircularProgress';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const styles = (theme) => ({
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+
+  loadProgressListItem: {
+    justifyContent: 'center',
+  },
+});
+
+
+const VirtualizedList = (props) => {
+  const { classes } = props;
+
+  const loadMoreProgressIndicator = (
+    <ListItem className={props.classes.loadProgressListItem} key="loadMoreProgressIndicator">
+      <CircularProgress color="primary" size={30} thickness={5} />
+    </ListItem>
+  );
+
+  return (
+    <List className={classes.root}>
+      {(props.store && props.store.items) ? (
+        <InfiniteScroll
+          initialLoad={false}
+          pageStart={1}
+          loadMore={(page) => { props.store.loadMore(page); }}
+          hasMore={!props.store.isLoaded}
+          loader={loadMoreProgressIndicator}
+        >
+          {props.store.items.map((item) => (
+            <props.componentForItem
+              key={item.id}
+              item={item}
+            />
+          ))}
+        </InfiniteScroll>
+      ) : (
+        <React.Fragment>
+          {loadMoreProgressIndicator}
+        </React.Fragment>
+      )}
+    </List>
+  );
+};
+
+VirtualizedList.propTypes = {
+  classes: PropTypes.object,
+  componentForItem: PropTypes.func.isRequired,
+  store: PropTypes.object,
+};
+
+export default withStyles(styles)(observer(VirtualizedList));
