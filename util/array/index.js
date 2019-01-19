0fb96f4e79052ce59a4856867d2a36f4e2557274
Allan Hart <allan.w.hart@gmail.com>
2019-01-19 12:04:19 +0800

    Added array utils

---
+++ util/array/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "arrayToObject", {
+  enumerable: true,
+  get: function get() {
+    return _object.arrayToObject;
+  },
+});
+
+const _array = _interopRequireDefault(require("./array"));
