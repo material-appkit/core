57265f6d5699e76da7d3a4ad53c366b508872522
Allan Hart <allan.w.hart@gmail.com>
2019-01-20 10:04:09 +0800

    Added NavManager

---
+++ managers/NavManager.js
@@ -0,0 +1,118 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _queryString = require('query-string');
+
+var _queryString2 = _interopRequireDefault(_queryString);
+
+var _history = require('history');
+
+var _mobxReactRouter = require('mobx-react-router');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var NavManager = function () {
+  function NavManager() {
+    _classCallCheck(this, NavManager);
+  }
+
+  _createClass(NavManager, null, [{
+    key: 'initialize',
+    value: function initialize() {
+      this.routerStore = new _mobxReactRouter.RouterStore();
+      this.history = (0, _mobxReactRouter.syncHistoryWithStore)((0, _history.createBrowserHistory)({ basename: process.env.REACT_APP_URL_BASENAME }), this.routerStore);
+    }
+
+    /**
+     * Return an object representation of the current query string
+     */
+
+  }, {
+    key: 'goBack',
+    value: function goBack() {
+      this.routerStore.history.goBack();
+    }
+  }, {
+    key: 'reloadWindow',
+    value: function reloadWindow() {
+      window.location.reload();
+    }
+
+    /**
+     * Replace ALL location querystring parameters with those provided, (excluding those without values).
+     * @param params Set of parameters to replace the URL with
+     * @param pathname Path to set on location. If undefined, use the current pathname.
+     * @param replace If set, replace the topmost URL in the history stack. Else push a new one.
+     */
+
+  }, {
+    key: 'setUrlParams',
+    value: function setUrlParams(params, pathname, replace) {
+      var qsParams = params || _queryString2.default.parse(this.routerStore.location.search);
+
+      // Filter out any parameters with unset values
+      var filteredParams = {};
+      Object.keys(qsParams).forEach(function (paramName) {
+        var value = qsParams[paramName];
+        if (value) {
+          filteredParams[paramName] = value;
+        }
+      });
+
+      var currentPathname = this.routerStore.location.pathname;
+      var querystring = _queryString2.default.stringify(filteredParams);
+      var url = (pathname || currentPathname) + '?' + querystring;
+
+      if (replace) {
+        this.routerStore.replace(url);
+      } else {
+        this.routerStore.push(url);
+      }
+    }
+
+    /**
+     * Convenience method to add, change, or remove params from the query string
+     * @param change Object containing params to add, change, or remove
+     */
+
+  }, {
+    key: 'updateUrlParams',
+    value: function updateUrlParams(change, replace) {
+      var params = this.qsParams;
+
+      Object.keys(change).forEach(function (key) {
+        var paramValue = change[key];
+        if (paramValue) {
+          params[key] = paramValue;
+        } else {
+          delete params[key];
+        }
+      });
+
+      this.setUrlParams(params, null, replace);
+    }
+  }, {
+    key: 'qsParams',
+    get: function get() {
+      return _queryString2.default.parse(this.routerStore.location.search);
+    }
+  }, {
+    key: 'currentLocation',
+    get: function get() {
+      return this.routerStore.location;
+    }
+  }]);
+
+  return NavManager;
+}();
+
+NavManager.routerStore = null;
+NavManager.history = null;
+exports.default = NavManager;
---
+++ src/managers/NavManager.js
@@ -0,0 +1,86 @@
+import qs from 'query-string';
+import { createBrowserHistory } from 'history';
+import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
+
+class NavManager {
+  static routerStore = null;
+  static history = null;
+
+  static initialize() {
+    this.routerStore = new RouterStore();
+    this.history = syncHistoryWithStore(
+      createBrowserHistory({ basename: process.env.REACT_APP_URL_BASENAME }),
+      this.routerStore
+    );
+  }
+
+  /**
+   * Return an object representation of the current query string
+   */
+  static get qsParams() {
+    return qs.parse(this.routerStore.location.search);
+  }
+
+  static get currentLocation() {
+    return this.routerStore.location;
+  }
+
+  static goBack() {
+    this.routerStore.history.goBack();
+  }
+
+  static reloadWindow() {
+    window.location.reload();
+  }
+
+  /**
+   * Replace ALL location querystring parameters with those provided, (excluding those without values).
+   * @param params Set of parameters to replace the URL with
+   * @param pathname Path to set on location. If undefined, use the current pathname.
+   * @param replace If set, replace the topmost URL in the history stack. Else push a new one.
+   */
+  static setUrlParams(params, pathname, replace) {
+    const qsParams = params || qs.parse(this.routerStore.location.search);
+
+    // Filter out any parameters with unset values
+    const filteredParams = {};
+    Object.keys(qsParams).forEach((paramName) => {
+      const value = qsParams[paramName];
+      if (value) {
+        filteredParams[paramName] = value;
+      }
+    });
+
+    const currentPathname = this.routerStore.location.pathname;
+    const querystring = qs.stringify(filteredParams);
+    const url = `${pathname || currentPathname}?${querystring}`;
+
+    if (replace) {
+      this.routerStore.replace(url);
+    } else {
+      this.routerStore.push(url);
+    }
+  }
+
+  /**
+   * Convenience method to add, change, or remove params from the query string
+   * @param change Object containing params to add, change, or remove
+   */
+  static updateUrlParams(change, replace) {
+    const params = this.qsParams;
+
+    Object.keys(change).forEach((key) => {
+      const paramValue = change[key];
+      if (paramValue) {
+        params[key] = paramValue;
+      } else {
+        delete params[key];
+      }
+    });
+
+    this.setUrlParams(params, null, replace);
+  }
+}
+
+
+export default NavManager;
