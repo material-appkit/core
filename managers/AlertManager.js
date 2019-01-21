2473b232f1c4718881e9aaba136275fac92a2431
Allan Hart <allan.w.hart@gmail.com>
2019-01-21 17:29:08 +0800

    Introduced AlertManager

---
+++ managers/AlertManager.js
@@ -0,0 +1,187 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _mobx = require('mobx');
+
+var _mobxReact = require('mobx-react');
+
+var _Button = require('@material-ui/core/Button');
+
+var _Button2 = _interopRequireDefault(_Button);
+
+var _Dialog = require('@material-ui/core/Dialog');
+
+var _Dialog2 = _interopRequireDefault(_Dialog);
+
+var _DialogActions = require('@material-ui/core/DialogActions');
+
+var _DialogActions2 = _interopRequireDefault(_DialogActions);
+
+var _DialogContent = require('@material-ui/core/DialogContent');
+
+var _DialogContent2 = _interopRequireDefault(_DialogContent);
+
+var _DialogContentText = require('@material-ui/core/DialogContentText');
+
+var _DialogContentText2 = _interopRequireDefault(_DialogContentText);
+
+var _DialogTitle = require('@material-ui/core/DialogTitle');
+
+var _DialogTitle2 = _interopRequireDefault(_DialogTitle);
+
+var _withStyles = require('@material-ui/core/styles/withStyles');
+
+var _withStyles2 = _interopRequireDefault(_withStyles);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /**
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               *
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               * AlertManager
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               *
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               */
+
+var AlertManager = function (_React$Component) {
+  _inherits(AlertManager, _React$Component);
+
+  function AlertManager() {
+    _classCallCheck(this, AlertManager);
+
+    return _possibleConstructorReturn(this, (AlertManager.__proto__ || Object.getPrototypeOf(AlertManager)).apply(this, arguments));
+  }
+
+  _createClass(AlertManager, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        _react2.default.Fragment,
+        null,
+        this.dialogs
+      );
+    }
+  }, {
+    key: 'dialogs',
+    get: function get() {
+      var _this2 = this;
+
+      var dialogs = [];
+      AlertManager.queue.forEach(function (alertInfo, key) {
+        dialogs.push(_react2.default.createElement(
+          _Dialog2.default,
+          {
+            key: key,
+            open: true,
+            onClose: _this2.handleClose,
+            'aria-labelledby': 'alert-dialog-title',
+            'aria-describedby': 'alert-dialog-description'
+          },
+          _react2.default.createElement(
+            _DialogTitle2.default,
+            { id: 'alert-dialog-title' },
+            alertInfo.title
+          ),
+          _react2.default.createElement(
+            _DialogContent2.default,
+            null,
+            _react2.default.createElement(
+              _DialogContentText2.default,
+              { id: 'alert-dialog-description' },
+              alertInfo.description
+            )
+          ),
+          _react2.default.createElement(
+            _DialogActions2.default,
+            null,
+            _react2.default.createElement(
+              _Button2.default,
+              { onClick: function onClick() {
+                  AlertManager.dismiss(key, false);
+                } },
+              'Cancel'
+            ),
+            _react2.default.createElement(
+              _Button2.default,
+              { onClick: function onClick() {
+                  AlertManager.dismiss(key, true);
+                }, color: 'primary' },
+              'OK'
+            )
+          )
+        ));
+      });
+      return dialogs;
+    }
+  }], [{
+    key: 'alert',
+    value: function alert(alertInfo) {
+      var key = new Date().getTime();
+      this.queue.set(key, alertInfo);
+    }
+  }, {
+    key: 'dismiss',
+    value: function () {
+      var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(key, flag) {
+        var alertInfo;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                alertInfo = this.queue.get(key);
+
+                if (!alertInfo.onDismiss) {
+                  _context.next = 4;
+                  break;
+                }
+
+                _context.next = 4;
+                return alertInfo.onDismiss(flag);
+
+              case 4:
+                this.queue.delete(key);
+
+              case 5:
+              case 'end':
+                return _context.stop();
+            }
+          }
+        }, _callee, this);
+      }));
+
+      function dismiss(_x, _x2) {
+        return _ref.apply(this, arguments);
+      }
+
+      return dismiss;
+    }()
+  }]);
+
+  return AlertManager;
+}(_react2.default.Component);
+
+AlertManager.queue = _mobx.observable.map();
+
+
+AlertManager.propTypes = {
+  classes: _propTypes2.default.object.isRequired
+};
+
+exports.default = (0, _withStyles2.default)({})((0, _mobxReact.observer)(AlertManager));
---
+++ src/managers/AlertManager.js
@@ -0,0 +1,84 @@
+/**
+*
+* AlertManager
+*
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import withStyles from '@material-ui/core/styles/withStyles';
+
+class AlertManager extends React.Component {
+  static queue = observable.map();
+
+  static alert(alertInfo) {
+    const key = new Date().getTime();
+    this.queue.set(key, alertInfo);
+  }
+
+  static async dismiss(key, flag) {
+    const alertInfo = this.queue.get(key);
+    if (alertInfo.onDismiss) {
+      await alertInfo.onDismiss(flag);
+    }
+    this.queue.delete(key);
+  }
+
+  get dialogs() {
+    const dialogs = [];
+    AlertManager.queue.forEach((alertInfo, key) => {
+      dialogs.push((
+        <Dialog
+          key={key}
+          open
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{alertInfo.title}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {alertInfo.description}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => { AlertManager.dismiss(key, false); }}>
+              Cancel
+            </Button>
+            <Button onClick={() => { AlertManager.dismiss(key, true); }} color="primary">
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+      ));
+    });
+    return dialogs;
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.dialogs}
+      </React.Fragment>
+    );
+  }
+}
+
+AlertManager.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles({
+
+})(observer(AlertManager));
