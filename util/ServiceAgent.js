4951d639c1163143fb66362828cef7474647ecb2
Allan Hart <allan.w.hart@gmail.com>
2019-01-23 13:40:11 +0800

    Factored out app-specific token refresh logic

--- util/ServiceAgent.js
+++ util/ServiceAgent.js
@@ -15,44 +15,7 @@ class ServiceAgent {
     }
   }
 
-  static getRefreshToken() {
-    const cookieName = process.env.REACT_APP_REFRESH_TOKEN_COOKIE_NAME;
-    return cookie.get(cookieName);
-  }
-  static setRefreshToken(value) {
-    const cookieName = process.env.REACT_APP_REFRESH_TOKEN_COOKIE_NAME;
-    if (value) {
-      cookie.set(cookieName, value);
-    } else {
-      cookie.remove(cookieName);
-    }
-  }
-
-
-  static async refreshAccessToken() {
-    const refreshToken = this.getRefreshToken();
-    if (!refreshToken) {
-      throw new Error('No refresh token available!');
-    }
-
-    try {
-      // If a refresh token is present, attempt to refresh the access token
-      const refreshTokenEndpoint =  process.env.REACT_APP_REFRESH_TOKEN_ENDPOINT;
-      const params = { refresh: refreshToken };
-      const res = await this._request('POST', refreshTokenEndpoint, params);
-
-      this.setAccessToken(res.body.access);
-      this.setRefreshToken(res.body.refresh);
-
-      return res;
-    } catch (err) {
-      this.setAccessToken(null);
-      this.setRefreshToken(null);
-      throw new Error('Access token refresh failed');
-    }
-  }
-
-  static _request(method, endpoint, params, context) {
+  static request(method, endpoint, params, context) {
     // Construct the AJAX request with the given params
     let requestURL = process.env.REACT_APP_API_URL;
     if (!endpoint.startsWith('/')) {
@@ -112,23 +75,6 @@ class ServiceAgent {
     return req;
   }
 
-  static async request(method, endpoint, params, context) {
-    try {
-      return await this._request(method, endpoint, params, context);
-    } catch (err) {
-      // If a requests fails for ANY reason, attempt to refresh the access
-      // token then re-issue.
-      // TODO: Distinguish requests that failed due to an invalid token.
-      // Only THEN do we want to clear the stored accessToken
-      // this.setAccessToken(null);
-      try {
-        await this.refreshAccessToken();
-        return await this._request(method, endpoint, params, context);
-      } catch (tokenRefreshError) {
-        throw err;
-      }
-    }
-  }
 
   static get(endpoint, params, context) {
     return this.request('GET', endpoint, params, context);
