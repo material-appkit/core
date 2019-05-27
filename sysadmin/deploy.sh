fcea8a84782891e7d44e02b555a15d472a3073b8
Allan Hart <allan.w.hart@gmail.com>
2019-05-27 09:30:33 -0600

    Added a deployment script to handle publishing to NPM

---
+++ sysadmin/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SYSADMIN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_PATH=$SYSADMIN_PATH/..
+BUILD_PATH=$PROJECT_PATH/build
+
+# Transpile the source to ES5
+cd $PROJECT_PATH
+npm run build
+
+# Insert the package.json containing necessary package info
+cp $PROJECT_PATH/package.deployment.json $BUILD_PATH/package.json
+
+# Publish the package to NPM
+cd $BUILD_PATH
+npm publish
+
+# Clean up the build products
+rm -rf $BUILD_PATH
