3e41e5cc1d27faf6798f1c49241766b553397788
Allan Hart <allan.w.hart@gmail.com>
2019-06-09 12:01:06 -0600

    Import makeStyles from core/styles

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -15,7 +15,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles }from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import withStyles from '@material-ui/core/styles/withStyles';
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
--- src/components/MetadataList.js
+++ src/components/MetadataList.js
@@ -11,7 +11,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { valueForKeyPath } from '../util/object';
 
