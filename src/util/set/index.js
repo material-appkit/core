7e03f56cf08a1da500b624f078b766e33827e105
Allan Hart <allan.w.hart@gmail.com>
2019-06-10 09:09:59 -0600

    Added set utility functions

---
+++ src/util/set/index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "isSuperset", {
+  enumerable: true,
+  get: function get() {
+    return _set.isSuperset;
+  },
+});
+
+Object.defineProperty(exports, "union", {
+  enumerable: true,
+  get: function get() {
+    return _set.union;
+  },
+});
+
+Object.defineProperty(exports, "intersection", {
+  enumerable: true,
+  get: function get() {
+    return _set.intersection;
+  },
+});
+
+Object.defineProperty(exports, "symmetricDifference", {
+  enumerable: true,
+  get: function get() {
+    return _set.symmetricDifference;
+  },
+});
+
+Object.defineProperty(exports, "difference", {
+  enumerable: true,
+  get: function get() {
+    return _set.difference;
+  },
+});
+
+const _set = _interopRequireDefault(require("./set"));
