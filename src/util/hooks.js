3d111fa43f92e92725f2aa48c2c0a373a9d38f1d
Allan Hart <allan.w.hart@gmail.com>
2019-10-08 17:07:24 -0700

    Added hook to assist with tracing

--- src/util/hooks.js
+++ src/util/hooks.js
@@ -39,3 +39,24 @@ export function useWidth() {
     }, null) || 'xs'
   );
 }
+
+
+/**
+ * Utility hook to help determine why/when a component is re-rendered
+ * @param props
+ */
+export function useTraceUpdate(props) {
+  const prev = useRef(props);
+  useEffect(() => {
+    const changedProps = Object.entries(props).reduce((ps, [k, v]) => {
+      if (prev.current[k] !== v) {
+        ps[k] = [prev.current[k], v];
+      }
+      return ps;
+    }, {});
+    if (Object.keys(changedProps).length > 0) {
+      console.log('Changed props:', changedProps);
+    }
+    prev.current = props;
+  });
+}
