cb85eb6d444004012f17d15cb960d98e4520dfdc
Allan Hart <allan.w.hart@gmail.com>
2019-11-05 10:09:03 +0800

    Import useTheme from core/styles

--- src/util/hooks.js
+++ src/util/hooks.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-import { useTheme } from '@material-ui/styles';
+import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 export function usePrevious(value) {
