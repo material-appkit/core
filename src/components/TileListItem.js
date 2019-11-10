2a579142d9f2a4b0a14e360ef3ff0eb1e5d761d6
Allan Hart <allan.w.hart@gmail.com>
2019-11-10 11:00:49 +0800

    Updated appearance of selection icon

--- src/components/TileListItem.js
+++ src/components/TileListItem.js
@@ -4,24 +4,36 @@
 *
 */
 
+import classNames from 'classnames';
+
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
+import CheckCircleTwoToneIcon from '@material-ui/icons/CheckCircleTwoTone';
 
 const styles = makeStyles((theme) => ({
   box: {
     cursor: 'pointer',
-    display: 'table-cell',
+    position: 'relative',
   },
 
-  selectionBox: {
-    border: `3px solid #F00`,
-    height: '100%',
+  selected: {
+    opacity: 0.6,
+  },
+
+  selectedIcon: {
+    border: '2px solid white',
+    borderRadius: 15,
+    color: theme.palette.primary.main,
     position: 'absolute',
-    top: 0,
-    width: '100%',
+
+    width: 30,
+    height: 30,
+
+    bottom: theme.spacing(1),
+    right: theme.spacing(1),
   },
 }));
 
@@ -54,17 +66,23 @@ function TileListItem(props) {
 
   const classes = styles();
 
+  const tileClasses = [];
+  if (selected) {
+    tileClasses.push(classes.selected);
+  }
+
   return (
     <Box
       className={classes.box}
       {...listTileProps}
       {...rest}
     >
-      {props.children}
+      <span className={classNames(tileClasses)}>
+        {props.children}
+      </span>
 
       {selected &&
-        <Box className={classes.selectionBox}>
-        </Box>
+        <CheckCircleTwoToneIcon className={classes.selectedIcon} />
       }
 
     </Box>
