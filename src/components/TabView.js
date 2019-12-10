7428995c4415d16c7ec734fb236cbe57b0bb48ac
Allan Hart <allan.w.hart@gmail.com>
2019-12-10 11:46:12 +0800

    Converted TabView to functional component

--- src/components/TabView.js
+++ src/components/TabView.js
@@ -1,86 +1,97 @@
-/**
-*
-* TabView
-*
-*/
-
 import PropTypes from 'prop-types';
-import React, { Fragment } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 import SplitView from './SplitView';
 
-class TabView extends React.PureComponent {
-  get selectedTabIndex() {
-    let selectedTabIndex = 0;
-    const currentLocationPath = this.props.location.pathname;
-    this.props.tabArrangement.forEach((tabConfig, tabIndex) => {
-      if (currentLocationPath === tabConfig.path) {
-        selectedTabIndex = tabIndex;
-      }
-    });
-    return selectedTabIndex;
-  }
+const styles = makeStyles((theme) => ({
+  tabs: {
+    backgroundColor: theme.palette.grey[50],
+    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+  },
+}));
 
-  render() {
-    const { classes, ...rest } = this.props;
 
-    const selectedTabIndex = this.selectedTabIndex;
-    const activeTabConfig = this.props.tabArrangement[selectedTabIndex];
+function TabView(props) {
+  const {
+    onConfig,
+    onSave,
+    tabArrangement,
+    ...rest
+  } = props;
 
-    const activeTabProps = activeTabConfig.componentProps || {};
-    activeTabProps.mountPath = activeTabConfig.path;
 
-    return (
-      <SplitView
-        bar={(
-          <Tabs
-            value={this.selectedTabIndex}
-            className={classes.tabs}
-            indicatorColor="primary"
-            scrollButtons="auto"
-            textColor="primary"
-            variant="scrollable"
-          >
-            {this.props.tabArrangement.map((tabConfig) => (
-              <Tab
-                key={tabConfig.path}
-                component={Link}
-                to={tabConfig.path}
-                label={tabConfig.label}
-              />
-            ))}
-          </Tabs>
-        )}
-        barSize={48}
-        placement="top"
-        scrollContent
-      >
-        <activeTabConfig.component
-          {...activeTabProps}
+  let initialTabIndex = 0;
+  const currentLocationPath = props.location.pathname;
+  tabArrangement.forEach((tabConfig, tabIndex) => {
+    if (currentLocationPath === tabConfig.path) {
+      initialTabIndex = tabIndex;
+    }
+  });
+
+  const [selectedTabIndex, setSelectedTabIndex] = useState(initialTabIndex);
+
+  useEffect(() => {
+    if (onConfig) {
+      onConfig({
+        selectedTabIndex
+      });
+    }
+
+  }, [selectedTabIndex]);
+
+  const activeTabConfig = tabArrangement[selectedTabIndex];
+  const activeTabProps = activeTabConfig.componentProps || {};
+  activeTabProps.mountPath = activeTabConfig.path;
+
+
+  const classes = styles();
+
+  return (
+    <SplitView
+      bar={(
+        <Tabs
+          value={selectedTabIndex}
+          className={classes.tabs}
+          indicatorColor="primary"
+          onChange={(e, index) => { setSelectedTabIndex(index); }}
+          scrollButtons="auto"
+          textColor="primary"
+          variant="scrollable"
           {...rest}
-        />
-      </SplitView>
-    );
-  }
+        >
+          {tabArrangement.map((tabConfig) => (
+            <Tab
+              key={tabConfig.path}
+              component={Link}
+              to={tabConfig.path}
+              label={tabConfig.label}
+            />
+          ))}
+        </Tabs>
+      )}
+      barSize={48}
+      placement="top"
+      scrollContent
+    >
+      <activeTabConfig.component
+        onSave={onSave}
+        {...activeTabProps}
+        {...rest}
+      />
+    </SplitView>
+  );
+
 }
 
 TabView.propTypes = {
-  classes: PropTypes.object.isRequired,
   location: PropTypes.object.isRequired,
+  onSave: PropTypes.func,
   tabArrangement: PropTypes.array.isRequired,
 };
 
-export default withStyles({
-  tabs: {
-    backgroundColor: '#fafafa',
-    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-  },
-  tab: {
-    padding: '0 10px',
-  },
-})(TabView);
+export default TabView;
