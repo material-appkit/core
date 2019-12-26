5ed1160c3e244295d287a7c9be8e752f657e707e
Allan Hart <allan.w.hart@gmail.com>
2019-12-27 07:29:25 +0800

    Support internal links within ContextMenu

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { Link as RouterLink } from 'react-router-dom';
+
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import Link from '@material-ui/core/Link';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 const styles = makeStyles((theme) => ({
@@ -44,27 +45,27 @@ function ContextMenu(props) {
   return (
     <Menu {...menuProps}>
       {menuItemArrangement.map((menuItemInfo) => {
-        const allMenuItemProps = {
-          ...menuItemProps,
+        const menuItemProps = {
           onClick: (e) => { handleMenuItemClick(e, menuItemInfo); }
         };
+
         if (menuItemInfo.href) {
-          allMenuItemProps.component = Link;
-          allMenuItemProps.href = menuItemInfo.href;
-          allMenuItemProps.target= '_blank';
-          allMenuItemProps.rel = 'noopener noreferrer';
+          menuItemProps.component = Link;
+          menuItemProps.href = menuItemInfo.href;
+          menuItemProps.target= '_blank';
+          menuItemProps.rel = 'noopener noreferrer';
         }
 
         if (menuItemInfo.link) {
-          allMenuItemProps.to = menuItemInfo.link;
-          allMenuItemProps.component = Link;
+          menuItemProps.to = menuItemInfo.link;
+          menuItemProps.component = RouterLink;
         }
 
         return (
           <MenuItem
             dense={dense}
             key={menuItemInfo.key}
-            {...allMenuItemProps}
+            {...menuItemProps}
           >
             {menuItemInfo.icon &&
               <ListItemIcon className={classes.listItemIcon}>
@@ -83,11 +84,9 @@ ContextMenu.propTypes = {
   dense: PropTypes.bool,
   onClose: PropTypes.func.isRequired,
   menuItemArrangement: PropTypes.array.isRequired,
-  menuItemProps: PropTypes.object,
 };
 
 ContextMenu.defaultProps = {
   dense: true,
-  menuItemProps: { }
 };
 export default ContextMenu;
