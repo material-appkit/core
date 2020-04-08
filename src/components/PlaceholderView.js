c0603259b8f3f2f0ca28e5fb9229f4d854936f00
allan <allan.w.hart@gmail.com>
2020-04-08 12:39:37 -0600

    Support dynamic sizing via aspectRatio

--- src/components/PlaceholderView.js
+++ src/components/PlaceholderView.js
@@ -7,7 +7,7 @@ import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 
 const styles = makeStyles((theme) => ({
-  content: {
+  contentContainer: {
     width: '100%',
     height: '100%',
     display: 'flex',
@@ -15,31 +15,59 @@ const styles = makeStyles((theme) => ({
     justifyContent: 'center',
   },
 
-  borderedContent: {
+  contentContainerUseAspectRatio: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+
+  contentContainerUseBorder: {
     border: `3px dashed ${theme.palette.grey[600]}`,
     borderRadius: theme.shape.borderRadius,
-  }
+  },
 }));
 
 const PlaceholderView = React.forwardRef((props, ref) => {
   const classes = styles();
 
   const {
+    aspectRatio,
     border,
     children,
     component,
+    fullHeight,
+    fullWidth,
+    height,
     padding,
     ...componentProps
   } = props;
 
   const Component = component;
-  const classNames = [classes.content];
+  const classNames = [classes.contentContainer];
   if (border) {
-    classNames.push(classes.borderedContent);
+    classNames.push(classes.contentContainerUseBorder);
+  }
+
+  const boxProps = { p: padding };
+
+  if (fullWidth) {
+    boxProps.width = "100%";
+  }
+
+  if (aspectRatio) {
+    boxProps.paddingBottom = `${100 / aspectRatio}%`;
+    boxProps.position = 'relative';
+    classNames.push(classes.contentContainerUseAspectRatio);
+  } else {
+    if (height) {
+      boxProps.height = height;
+    } else if (fullHeight) {
+      boxProps.height = "100%";
+    }
   }
 
   return (
-    <Box width="100%" height="100%" p={padding}>
+    <Box {...boxProps}>
       <Component
         className={cx(classNames)}
         ref={ref}
@@ -52,15 +80,21 @@ const PlaceholderView = React.forwardRef((props, ref) => {
 });
 
 PlaceholderView.propTypes = {
+  aspectRatio: PropTypes.number,
   border: PropTypes.bool,
   children: PropTypes.node,
   component: PropTypes.any,
+  fullHeight: PropTypes.bool,
+  fullWidth: PropTypes.bool,
+  height: PropTypes.number,
   padding: PropTypes.number,
 };
 
 PlaceholderView.defaultProps = {
   border: true,
   component: 'div',
+  fullHeight: true,
+  fullWidth: true,
   padding: 0,
 };
 
