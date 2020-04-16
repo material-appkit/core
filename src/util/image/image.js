bc5a0f176172a7dbbc2bf931d6f08389c83d9470
allan <allan.w.hart@gmail.com>
2020-04-16 08:07:15 -0600

    Introduced utility to resize an image and return as data or blob

---
+++ src/util/image/image.js
@@ -0,0 +1,54 @@
+/**
+ * @param image
+ * @param options
+ * @returns {Promise}
+ */
+export function resize(sourceImage, options) {
+  return new Promise((resolve, reject) => {
+    const resizedImage = new Image();
+    resizedImage.src = sourceImage;
+    resizedImage.onload = () => {
+      const sourceWidth = resizedImage.width;
+      const sourceHeight = resizedImage.height;
+
+      let targetWidth = sourceWidth;
+      let targetHeight = sourceHeight;
+
+      const { maxSize } = options;
+      if (maxSize) {
+        if (sourceWidth >= sourceHeight) {
+          targetWidth = maxSize;
+          targetHeight = sourceHeight * (maxSize / sourceWidth);
+        } else {
+          targetHeight = maxSize;
+          targetWidth = sourceWidth * (maxSize / sourceHeight);
+        }
+      }
+
+      const elem = document.createElement('canvas');
+      elem.width = targetWidth;
+      elem.height = targetHeight;
+
+      const ctx = elem.getContext('2d');
+      ctx.drawImage(resizedImage, 0, 0, targetWidth, targetHeight);
+
+      const format = options.format || 'data';
+      const mimetype = options.mimetype || 'image/png';
+      const quality = options.quality || 1;
+
+      switch (format) {
+        case 'data':
+          const resizedImageData = ctx.canvas.toDataURL(resizedImage, mimetype, quality);
+          resolve(resizedImageData);
+          break;
+        case 'blob':
+          ctx.canvas.toBlob((resizedImageBlob) => {
+            resolve(resizedImageBlob);
+          }, mimetype, quality);
+          break;
+        default:
+          throw new Error(`Unsupported output format: ${format}`);
+      }
+    };
+  });
+}
---
+++ src/util/image/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "resize", {
+  enumerable: true,
+  get: function get() {
+    return _image.resize;
+  },
+});
+
+const _image = _interopRequireDefault(require("./image"));
