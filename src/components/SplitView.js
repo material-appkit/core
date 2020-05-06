b2d7508c3f4fcb04353eefd0379e57a161c89116
allan <allan.w.hart@gmail.com>
2020-05-06 09:28:56 -0600

    Restored absolute positioning behavior

--- src/components/SplitView.js
+++ src/components/SplitView.js
@@ -6,40 +6,115 @@ function SplitView(props) {
     bar,
     barSize,
     children,
+    layout,
     placement,
     scrollContent,
   } = props;
 
   const splitViewStyle = {
-    display: 'grid',
     height: '100%',
   };
 
-
-  const barStyles = {};
   const contentStyles = {};
+  const barStyles = {};
 
-  switch (placement) {
-      case 'top':
-        splitViewStyle.gridTemplateRows = `${barSize}px auto`;
-        break;
-      case 'bottom':
-        splitViewStyle.gridTemplateRows = `auto ${barSize}px`;
-        contentStyles.order = 0;
-        barStyles.order = 1;
-        break;
-      case 'left':
-        splitViewStyle.gridTemplateColumns = `${barSize}px auto`;
-        break;
-      case 'right':
-        splitViewStyle.gridTemplateColumns = `auto ${barSize}px`;
-        contentStyles.order = 0;
-        barStyles.order = 1;
-        break;
-  }
 
-  if (scrollContent) {
-    contentStyles.overflow = 'auto';
+  switch (layout) {
+    case 'grid':
+      splitViewStyle.display = 'grid';
+      contentStyles.overflow = 'auto';
+
+      switch (placement) {
+        case 'top':
+          splitViewStyle.gridTemplateRows = `${barSize}px auto`;
+          break;
+        case 'bottom':
+          splitViewStyle.gridTemplateRows = `auto ${barSize}px`;
+          contentStyles.order = 0;
+          barStyles.order = 1;
+          break;
+        case 'left':
+          splitViewStyle.gridTemplateColumns = `${barSize}px auto`;
+          break;
+        case 'right':
+          splitViewStyle.gridTemplateColumns = `auto ${barSize}px`;
+          contentStyles.order = 0;
+          barStyles.order = 1;
+          break;
+      }
+      break;
+    case 'position':
+      splitViewStyle.position = 'relative';
+      contentStyles.overflow = scrollContent ? 'auto' : 'visible';
+
+      switch (placement) {
+        case 'top':
+          Object.assign(barStyles, {
+            top: 0,
+            right: 0,
+            left: 0,
+            height: barSize,
+            position: 'absolute',
+          });
+          Object.assign(contentStyles, {
+            top: barSize,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            position: 'absolute',
+          });
+          break;
+        case 'bottom':
+          Object.assign(barStyles, {
+            bottom: 0,
+            right: 0,
+            left: 0,
+            height: barSize,
+            position: 'absolute',
+          });
+          Object.assign(contentStyles, {
+            top: 0,
+            right: 0,
+            left: 0,
+            position: 'absolute',
+          });
+          break;
+        case 'left':
+          Object.assign(barStyles, {
+            top: 0,
+            left: 0,
+            bottom: 0,
+            width: barSize,
+            position: 'absolute',
+          });
+          Object.assign(contentStyles, {
+            top: 0,
+            right: 0,
+            left: barSize,
+            bottom: 0,
+            position: 'absolute',
+          });
+          break;
+        case 'right':
+          Object.assign(barStyles, {
+            position: 'absolute',
+            top: 0,
+            width: barSize,
+            bottom: 0,
+            right: 0,
+          });
+          Object.assign(contentStyles, {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: barSize,
+            bottom: 0,
+          });
+          break;
+      }
+      break;
+    default:
+      throw new Error(`Unexpected layout property: ${layout}`);
   }
 
 
@@ -63,12 +138,14 @@ SplitView.propTypes = {
   children: PropTypes.object,
   classes: PropTypes.object,
   contentClassName: PropTypes.string,
+  layout: PropTypes.oneOf(['grid', 'position']),
   placement: PropTypes.oneOf(['top', 'right', 'bottom', 'left']),
   scrollContent: PropTypes.bool,
 };
 
 SplitView.defaultProps = {
-  scrollContent: true,
+  layout: 'position',
+  scrollContent: false,
 };
 
 export default SplitView;
