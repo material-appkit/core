ab10511dc5cb69bca10e986ac8f1fe2a09c252f2
allan <allan.w.hart@gmail.com>
2020-05-09 11:12:37 -0600

    Support Gatsby-style environment vars

--- src/util/ajax/ServiceProxy.js
+++ src/util/ajax/ServiceProxy.js
@@ -2,13 +2,49 @@ import cookie from 'js-cookie';
 import request from 'superagent';
 
 export default class ServiceProxy {
+  static getAccessTokenCookieName() {
+    const cookieName = (
+      process.env.REACT_APP_ACCESS_TOKEN_COOKIE_NAME ||
+      process.env.GATSBY_ACCESS_TOKEN_COOKIE_NAME
+    );
+    if (!cookieName) {
+      throw new Error('Expecting an environment variable of name "REACT_APP_ACCESS_TOKEN_COOKIE_NAME" or "GATSBY_ACCESS_TOKEN_COOKIE_NAME"');
+    }
+
+    return cookieName;
+  }
+
+  static getBaseURL() {
+    const baseUrl = (
+      process.env.REACT_APP_API_URL ||
+      process.env.GATSBY_API_URL
+    );
+    if (!baseUrl) {
+      throw new Error('Expecting an environment variable of name "REACT_APP_API_URL" or "GATSBY_API_URL"');
+    }
+
+    return baseUrl;
+  }
+
+  static getBaseURLPrefix() {
+    const urlPrefix = (
+      process.env.REACT_APP_API_ENDPOINT_PREFIX ||
+      process.env.GATSBY_API_ENDPOINT_PREFIX
+    );
+    if (!urlPrefix) {
+      throw new Error('Expecting an environment variable of name "REACT_APP_API_ENDPOINT_PREFIX" or "GATSBY_API_ENDPOINT_PREFIX"');
+    }
+
+    return urlPrefix;
+  }
+
   static getAccessToken() {
-    return cookie.get(process.env.REACT_APP_ACCESS_TOKEN_COOKIE_NAME);
+    return cookie.get(this.getAccessTokenCookieName());
   }
 
   static setAccessToken(value, attrs) {
     const cookieAttributes = attrs || {};
-    const cookieName = process.env.REACT_APP_ACCESS_TOKEN_COOKIE_NAME;
+    const cookieName = this.getAccessTokenCookieName();
     if (value) {
       cookie.set(cookieName, value, cookieAttributes);
     } else {
@@ -21,7 +57,7 @@ export default class ServiceProxy {
 
     if (typeof(endpointInfo) === 'string') {
       endpointInfo = {
-        url: process.env.REACT_APP_API_URL,
+        url: this.getBaseURL(),
         path: endpointInfo,
       }
     }
@@ -34,7 +70,7 @@ export default class ServiceProxy {
     // Construct the AJAX request with the given params
     let requestURL = endpointInfo.url;
     if (!endpointInfo.path.startsWith('/')) {
-      requestURL += process.env.REACT_APP_API_ENDPOINT_PREFIX;
+      requestURL += this.getBaseURLPrefix();
     }
     requestURL += endpointInfo.path;
 
