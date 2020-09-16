ccfaa488e2976f80d41fc311127e5e86f77f56bf
allan <allan.w.hart@gmail.com>
2020-09-16 09:58:45 -0600

    Factored ExpandableBox out of ChoiceList

--- src/components/ChoiceList.js
+++ src/components/ChoiceList.js
@@ -1,23 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import CheckBoxOutlinedBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 
+import ExpandableBox from './ExpandableBox';
+
 import { arrayToObject } from '../util/array';
-import { useInit } from '../util/hooks';
 
 const variantPropType = PropTypes.oneOf(['single', 'multiple']).isRequired;
 
@@ -81,41 +77,10 @@ ChoiceListItem.propTypes = {
 
 
 // -----------------------------------------------------------------------------
-
-const styles = makeStyles((theme) => ({
-  fieldLabel: {
-    ...theme.mixins.filterFieldLabel,
-
-    color: theme.palette.primary.main,
-    '&:hover': {
-      textDecoration: 'underline',
-    },
-  },
-  fieldValue: theme.mixins.filterFieldValue,
-
-  disclosureButton: {
-    justifyContent: 'flex-start',
-    padding: 0,
-  },
-
-  disclosureButtonStartIcon: {
-    margin: 0,
-  },
-}));
-
 function ChoiceList(props) {
-  const classes = styles();
-
   const { choices, value, variant } = props;
-  const [expanded, setExpanded] = useState(false);
   const [fieldValueLabel, setFieldValueLabel] = useState('Any');
   const [selection, setSelection] = useState(new Set());
-
-  useInit(() => {
-    if (value === null || (value && value.length)) {
-      setExpanded(true);
-    }
-  });
 
   useEffect(() => {
     let valueLabel = 'Any';
@@ -177,53 +142,31 @@ function ChoiceList(props) {
   };
 
   return (
-    <Box>
-      <Box display="flex" justifyContent="space-between" marginBottom={0.5}>
-        <Button
-          classes={{
-            root: classes.disclosureButton,
-            label: classes.fieldLabel,
-            startIcon: classes.disclosureButtonStartIcon,
-          }}
-          disableRipple
-          disableFocusRipple
-          onClick={() => setExpanded(!expanded)}
-          startIcon={expanded ? (
-            <ExpandMoreIcon />
-          ) : (
-            <ChevronRightIcon />
-          )}
-        >
-          {props.label}
-        </Button>
-
-        <Typography className={classes.fieldValue}>
-          {fieldValueLabel}
-        </Typography>
-      </Box>
-
-      {expanded &&
-        <List disablePadding>
-          {props.nullLabel &&
-            <ChoiceListItem
-              choice={{ value: null, label: props.nullLabel }}
-              onClick={handleNullListItemClick}
-              selected={selection === null}
-              variant={variant}
-            />
-          }
-          {choices.map((choice) => (
-            <ChoiceListItem
-              choice={choice}
-              key={choice.value}
-              onClick={handleValueListItemClick(choice)}
-              selected={selection && selection.has(choice.value)}
-              variant={variant}
-            />
-          ))}
-        </List>
-      }
-    </Box>
+    <ExpandableBox
+      defaultExpanded={Boolean(value === null || (value && value.length))}
+      label={props.label}
+      valueLabel={fieldValueLabel}
+    >
+      <List disablePadding>
+        {props.nullLabel &&
+          <ChoiceListItem
+            choice={{ value: null, label: props.nullLabel }}
+            onClick={handleNullListItemClick}
+            selected={selection === null}
+            variant={variant}
+          />
+        }
+        {choices.map((choice) => (
+          <ChoiceListItem
+            choice={choice}
+            key={choice.value}
+            onClick={handleValueListItemClick(choice)}
+            selected={selection && selection.has(choice.value)}
+            variant={variant}
+          />
+        ))}
+      </List>
+    </ExpandableBox>
   );
 }
 
