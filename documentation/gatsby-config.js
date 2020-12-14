828095fb02fd4e0a2512f3ad188a9c0ce5d652d5
Allan Hart <allan.w.hart@gmail.com>
2020-12-14 15:27:01 -0700

    Restored image used to generate app manifest

--- documentation/gatsby-config.js
+++ documentation/gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         background_color: '#fafafa',
         cache_busting_mode: 'none',
         display: 'standalone',
-        icon: 'src/images/application-logo.png',
+        icon: 'src/images/favicon-192x192.png',
         name: process.env.GATSBY_APP_TITLE,
         theme_color: '#fff',
         short_name: process.env.GATSBY_APP_TITLE,
