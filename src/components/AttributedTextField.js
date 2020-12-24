374ab415b8cdbe14d3806c83521a31c330b18b8a
Allan Hart <allan.w.hart@gmail.com>
2020-12-24 01:37:53 -0700

    Introduced new variant: 'contained'

--- src/components/AttributedTextField.js
+++ src/components/AttributedTextField.js
@@ -6,26 +6,45 @@ import React, { useEffect, useRef, useState } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import MuiTextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
 import CancelIcon from '@material-ui/icons/Cancel';
 
+const styles = makeStyles({
+  containedInputLabelShrink: {
+    transform: 'translate(14px, 8px) scale(0.75) !important',
+  },
+
+  containedInputInput: {
+    padding: `24px 14px 12px 14px`,
+  },
+});
+
 function AttributedTextField(props) {
+  const classes = styles();
+
   const {
     clearable,
+    InputLabelProps,
+    InputProps,
     onChange,
     onChangeDelay,
     StartIcon,
     value,
     valueTransformer,
+    variant,
     ...textFieldProps
   } = props;
 
   const [fieldValue, setFieldValue] = useState(value);
+
 
   useEffect(() => {
     if (value !== fieldValue) {
       updateFieldValue(value);
     }
   }, [value]);
+
 
   const searchTermChangeHandlerRef = useRef(
     debounce((v) => {
@@ -34,7 +53,7 @@ function AttributedTextField(props) {
   );
 
 
-  const updateFieldValue = (v) => {
+  const updateFieldValue = (v, e) => {
     let updatedValue = v;
     if (valueTransformer) {
       updatedValue = valueTransformer(updatedValue);
@@ -44,21 +63,42 @@ function AttributedTextField(props) {
     if (onChangeDelay) {
       searchTermChangeHandlerRef.current(updatedValue);
     } else {
-      onChange(updatedValue);
+      onChange(updatedValue, e);
     }
   };
 
-  const InputProps = {};
+
+  const FinalInputProps = InputProps ? {...InputProps} : {};
+  const FinalInputLabelProps = InputLabelProps ? {...InputLabelProps} : {};
+
   if (StartIcon) {
-    InputProps.startAdornment = (
+    FinalInputProps.startAdornment = (
       <InputAdornment position="start">
         <StartIcon />
       </InputAdornment>
     );
   }
 
+  let derivedVariant = variant;
+  if (derivedVariant === 'contained') {
+    FinalInputProps.notched = false;
+
+    if (FinalInputProps.classes) {
+      FinalInputProps.classes.input = classes.containedInputInput
+    } else {
+      FinalInputProps.classes = { input: classes.containedInputInput };
+    }
+
+    if (FinalInputLabelProps.classes) {
+      FinalInputLabelProps.classes.shrink = classes.containedInputLabelShrink
+    } else {
+      FinalInputLabelProps.classes = { shrink: classes.containedInputLabelShrink };
+    }
+    derivedVariant = 'outlined';
+  }
+
   if (clearable) {
-    InputProps.endAdornment = fieldValue ? (
+    FinalInputProps.endAdornment = fieldValue ? (
       <InputAdornment position="end">
         <IconButton
           edge="end"
@@ -73,9 +113,11 @@ function AttributedTextField(props) {
 
   return (
     <MuiTextField
-      InputProps={InputProps}
-      onChange={(e) => updateFieldValue(e.target.value)}
+      InputProps={FinalInputProps}
+      InputLabelProps={FinalInputLabelProps}
+      onChange={(e) => updateFieldValue(e.target.value, e)}
       value={fieldValue}
+      variant={derivedVariant}
       {...textFieldProps}
     />
   );
@@ -84,7 +126,6 @@ function AttributedTextField(props) {
 AttributedTextField.propTypes = {
   clearable: PropTypes.bool,
   onTimeout: PropTypes.func,
-  placeholder: PropTypes.string,
   onChangeDelay: PropTypes.number,
   StartIcon: PropTypes.elementType,
   value: PropTypes.oneOfType([
@@ -92,12 +133,12 @@ AttributedTextField.propTypes = {
     PropTypes.number,
   ]),
   valueTransformer: PropTypes.func,
-  variant: PropTypes.string,
+  variant: PropTypes.oneOf(['contained', 'filled', 'outlined', 'standard']),
 };
 
 AttributedTextField.defaultProps = {
-  clearable: true,
-  onChangeDelay: 300,
+  clearable: false,
+  onChangeDelay: 0,
   value: '',
   variant: 'standard',
 };
