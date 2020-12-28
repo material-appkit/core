b084e87210f35878c1f624a1ea16e8a577e79119
allan <allan.w.hart@gmail.com>
2020-12-28 13:20:53 -0700

    Adopted implementation of matchPath

--- src/util/urls/urls.js
+++ src/util/urls/urls.js
@@ -1,9 +1,51 @@
 import { pathToRegexp, compile } from 'path-to-regexp';
-import { matchPath } from 'react-router-dom';
 
 export function reverse(path, params) {
   const toPath = compile(path);
   return toPath(params);
+}
+
+/**
+ * Public API for matching a URL pathname to a path.
+ * Source: https://github.com/ReactTraining/react-router/blob/master/packages/react-router/modules/matchPath.js
+ */
+function matchPath(pathname, options = {}) {
+  if (typeof options === "string" || Array.isArray(options)) {
+    options = { path: options };
+  }
+
+  const { path, exact = false, strict = false, sensitive = false } = options;
+
+  const paths = [].concat(path);
+
+  return paths.reduce((matched, path) => {
+    if (!path && path !== "") return null;
+    if (matched) return matched;
+
+    const { regexp, keys } = compilePath(path, {
+      end: exact,
+      strict,
+      sensitive
+    });
+    const match = regexp.exec(pathname);
+
+    if (!match) return null;
+
+    const [url, ...values] = match;
+    const isExact = pathname === url;
+
+    if (exact && !isExact) return null;
+
+    return {
+      path, // the path used to match
+      url: path === "/" && url === "" ? "/" : url, // the matched portion of the URL
+      isExact, // whether or not we matched exactly
+      params: keys.reduce((memo, key, index) => {
+        memo[key.name] = values[index];
+        return memo;
+      }, {})
+    };
+  }, null);
 }
 
 
