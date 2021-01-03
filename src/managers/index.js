2e74cf908c02fb9e7569a9cfd637f213bd3ee604
allan <allan.w.hart@gmail.com>
2021-01-03 10:44:33 -0700

    Introduced index for managers and utility classes

---
+++ src/managers/index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+const _AlertManager = _interopRequireDefault(require("./AlertManager"));
+Object.defineProperty(exports, "AlertManager", {
+  enumerable: true,
+  get: function get() {
+    return _AlertManager.default;
+  },
+});
+
+
+const _NavManager = _interopRequireDefault(require("./NavManager"));
+Object.defineProperty(exports, "NavManager", {
+  enumerable: true,
+  get: function get() {
+    return _NavManager.default;
+  },
+});
+
+
+const _NotificationManager = _interopRequireDefault(require("./NotificationManager"));
+Object.defineProperty(exports, "NotificationManager", {
+  enumerable: true,
+  get: function get() {
+    return _NotificationManager.default;
+  },
+});
+
+
+const _SnackbarManager = _interopRequireDefault(require("./SnackbarManager"));
+Object.defineProperty(exports, "SnackbarManager", {
+  enumerable: true,
+  get: function get() {
+    return _SnackbarManager.default;
+  },
+});
+
+
+const _StorageManager = _interopRequireDefault(require("./StorageManager"));
+Object.defineProperty(exports, "StorageManager", {
+  enumerable: true,
+  get: function get() {
+    return _StorageManager.default;
+  },
+});
--- src/util/index.js
+++ src/util/index.js
@@ -2,6 +2,7 @@
 
 const _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
+const _ServiceAgent = _interopRequireDefault(require("./ServiceAgent"));
 Object.defineProperty(exports, "ServiceAgent", {
   enumerable: true,
   get: function get() {
@@ -9,4 +10,21 @@ Object.defineProperty(exports, "ServiceAgent", {
   },
 });
 
-const _ServiceAgent = _interopRequireDefault(require("./ServiceAgent"));
+
+const _Notification = _interopRequireDefault(require("./Notification"));
+Object.defineProperty(exports, "Notification", {
+  enumerable: true,
+  get: function get() {
+    return _Notification.default;
+  },
+});
+
+
+const _NotificationCenter = _interopRequireDefault(require("./NotificationCenter"));
+Object.defineProperty(exports, "NotificationCenter", {
+  enumerable: true,
+  get: function get() {
+    return _ServiceAgent.default;
+  },
+});
+
