671c49bb9079a5d7e3ecff067fb4930a427fc19c
Allan Hart <allan.w.hart@gmail.com>
2021-01-05 22:44:46 -0700

    Added docs to managers

--- src/managers/NavManager.js
+++ src/managers/NavManager.js
@@ -4,6 +4,7 @@ import { createBrowserHistory } from 'history';
 import { isSet } from '../util/value';
 
 /**
+ * @summary
  * General description about the NavManager
  */
 class NavManager {
@@ -11,20 +12,33 @@ class NavManager {
 
   /**
    *
-   * @param options
+   * @param {Object} options
+   *
+   * @static
    */
   static initialize(options) {
     const historyBaseName = options.basename || process.env.REACT_APP_URL_BASENAME;
     this.history = createBrowserHistory({ basename: historyBaseName });
   }
 
+  /**
+   * @static
+   *
+   * @returns {Object}
+   * Current location in browser history
+   *
+   * @static
+   */
   static get currentLocation() {
     return this.history.location;
   }
 
 
   /**
-   * Return an object representation of the current query string
+   * @returns {Object}
+   * An object representation of the current query string
+   *
+   * @static
    */
   static get qsParams() {
     return qs.parse(this.currentLocation.search);
@@ -32,17 +46,32 @@ class NavManager {
 
 
   /**
+   * @summary
    * Reload the page at the current location
+   *
+   * @static
    */
   static reloadWindow() {
     window.location.reload();
   }
 
   /**
+   * @summary
    * Replace ALL location querystring parameters with those provided, (excluding those without values).
-   * @param params Set of parameters to replace the URL with
-   * @param pathname Path to set on location. If undefined, use the current pathname.
-   * @param replace If set, replace the topmost URL in the history stack. Else push a new one.
+   *
+   * @param {Object} params
+   * Set of parameters to replace the URL with
+   *
+   * @param {String} pathname
+   * Path to set on location. If undefined, use the current pathname.
+   *
+   * @param {Boolean} replace
+   * If set, replace the topmost URL in the history stack. Else push a new one.
+   *
+   * @param {Object} state
+   * Optional context to include new location
+   *
+   * @static
    */
   static setUrlParams(params, pathname, replace, state) {
     const currentLocation = this.currentLocation;
@@ -69,10 +98,18 @@ class NavManager {
   }
 
   /**
+   *
+   * @summary
    * Convenience method to add or change a set of querystring params
-   * @param change Object containing params to add or change
-   * @param replace Flag indicating whether to add a new history entry or
-   *                update the current history state
+   *
+   * @param {Object} change
+   * Object containing params to add or change
+   *
+   * @param {Boolean} replace
+   * Flag indicating whether to add a new history entry or update
+   * the current history state
+   *
+   * @static
    */
   static updateUrlParams(change, replace) {
     const params = { ...this.qsParams, ...change };
@@ -81,7 +118,14 @@ class NavManager {
   }
 
   /**
+   * @summary
    * Convenience method to set a single querystring param
+   *
+   * @param {String} paramName
+   * @param {*} paramValue
+   * @param {Boolean} replace
+   *
+   * @static
    */
   static updateUrlParam(paramName, paramValue, replace) {
     this.updateUrlParams({ [paramName]: paramValue }, replace);
@@ -89,7 +133,13 @@ class NavManager {
 
 
   /**
+   * @summary
    * Convenience method to clear a set of querystring params
+   *
+   * @param {Array} paramNames
+   * @param {Boolean} replace
+   *
+   * @static
    */
   static clearUrlParams(paramNames, replace) {
     const params = { ...this.qsParams };
@@ -101,7 +151,13 @@ class NavManager {
 
 
   /**
+   * @summary
    * Convenience method to clear a single querystring param
+   *
+   * @param {String} paramName
+   * @param {Boolean} replace*
+   *
+   * @static
    */
   static clearUrlParam(paramName, replace) {
     this.clearUrlParams([paramName], replace);
@@ -109,15 +165,26 @@ class NavManager {
 
 
   /**
+   * @summary
    * Unlike the underlying setUrlParams method, this method will clear the
    * querystring params if qsParams is unset.
+   *
+   * @param {String} path
+   * @param {Object} qsParams
+   * @param {Boolean} replace
+   * @param {Boolean} state
+   *
+   * @static
    */
   static navigate(path, qsParams, replace, state) {
     this.setUrlParams(qsParams || {}, path, replace, state);
   }
 
   /**
+   * @summary
    * Navigate to the previous page in the location history
+   *
+   * @static
    */
   static goBack() {
     this.history.goBack();
--- src/managers/StorageManager.js
+++ src/managers/StorageManager.js
@@ -8,6 +8,8 @@ class StorageManager {
    *
    * @param {String} key
    * @returns {string|number|Date}
+   *
+   * @static
    */
   static localValue(key) {
     const storage = storageOfType('localStorage');
@@ -19,6 +21,8 @@ class StorageManager {
    *
    * @param {String} key
    * @param {string|number|Date} value
+   *
+   * @static
    */
   static setLocalValue(key, value) {
     const storage = storageOfType('localStorage');
@@ -32,6 +36,8 @@ class StorageManager {
   /**
    *
    * @param {String} key
+   *
+   * @static
    */
   static removeLocalValue(key) {
     const storage = storageOfType('localStorage');
