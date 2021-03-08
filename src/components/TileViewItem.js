b879f8ed446d0e3a2bffdfdac406dec194082989
allan <allan.w.hart@gmail.com>
2021-03-08 09:01:05 -0700

    Place context menu button in top-left of grid item

--- src/components/TileViewItem.js
+++ src/components/TileViewItem.js
@@ -1,16 +1,44 @@
 import clsx from 'clsx';
 
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-
 import CheckCircleTwoToneIcon from '@material-ui/icons/CheckCircleTwoTone';
 
+import ContextMenuButton from './ContextMenuButton';
+
 const styles = makeStyles((theme) => ({
-  selected: {
-    opacity: 0.6,
+  gridItem: {
+    position: 'relative',
+  },
+
+  contextMenuButton: {
+    position: 'absolute',
+    top: theme.spacing(1),
+    right: theme.spacing(1),
+
+    borderTopLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    borderTopRightRadius: 2,
+    borderBottomLeftRadius: 6,
+
+    transition: theme.transitions.create(['background-color'], {
+      duration: theme.transitions.duration.short,
+    }),
+
+    backgroundColor: theme.palette.grey[50],
+    '&:hover': {
+      backgroundColor: theme.palette.grey.A100,
+      // Reset on touch devices, it doesn't add specificity
+      '@media (hover: none)': {
+        backgroundColor: theme.palette.grey[300],
+      },
+      '&$disabled': {
+        backgroundColor: theme.palette.action.disabledBackground,
+      },
+    },
   },
 
   selectedIcon: {
@@ -29,7 +57,10 @@ const styles = makeStyles((theme) => ({
 
 
 function TileViewItem(props) {
+  const classes = styles();
+
   const {
+    className,
     commitOnSelect,
     secondaryActionPlacement,
     contextMenuItemArrangement,
@@ -48,39 +79,54 @@ function TileViewItem(props) {
     ...rest
   } = props;
 
-  const gridItemProps = {
+  const [contextMenuButton, setContextMenuButton] = useState(null);
+
+  const [gridItemProps] = useState(() => ({
     ...sizes,
-    ...rest
-  };
+    ...rest,
+    className: clsx(classes.gridItem, className),
+    onClick: (e) => {
+      if (selectionMode && onSelectionChange) {
+        onSelectionChange(item);
+      }
 
-  gridItemProps.onClick = (e) => {
-    if (selectionMode && onSelectionChange) {
-      onSelectionChange(item);
+      if (onItemClick) {
+        onItemClick(item, e);
+      }
     }
+  }));
 
-    if (onItemClick) {
-      onItemClick(item, e);
+
+  useEffect(() => {
+    if (contextMenuItemArrangement) {
+      let menuItemArrangement = contextMenuItemArrangement;
+      if (typeof(menuItemArrangement) === 'function') {
+        menuItemArrangement = menuItemArrangement(item);
+      }
+      setContextMenuButton(
+        <ContextMenuButton
+          buttonProps={{
+            className: classes.contextMenuButton,
+            size: 'small'
+          }}
+          representedObject={item}
+          menuItemArrangement={menuItemArrangement}
+        />
+      );
+    } else {
+      setContextMenuButton(null);
     }
-  };
-
-  const classes = styles();
-
-  const tileClasses = [];
-  if (selected) {
-    tileClasses.push(classes.selected);
-  }
+  }, [contextMenuItemArrangement]);
 
   return (
-    <Grid
-      className={clsx(tileClasses)}
-      item
-      {...gridItemProps}
-    >
+    <Grid item {...gridItemProps}>
       {props.children}
 
       {selected &&
         <CheckCircleTwoToneIcon className={classes.selectedIcon} />
       }
+
+      {contextMenuButton}
     </Grid>
   );
 }
@@ -106,5 +152,5 @@ TileViewItem.defaultProps = {
   sizes: { xs: 12, sm: 6, md: 4, lg: 3 },
 };
 
-export default TileViewItem;
+export default React.memo(TileViewItem);
 
