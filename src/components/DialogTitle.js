bf34c0c3e143df70cd1f99a4bf6e00e975989401
Allan Hart <allan.w.hart@gmail.com>
2021-09-30 13:48:29 -0300

    Introduced component: 'DialogTitle'

---
+++ src/components/DialogTitle.js
@@ -0,0 +1,77 @@
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+
+import React, { useCallback } from 'react';
+
+import MUIDialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import CloseIcon from '@material-ui/icons/Close';
+
+const styles = makeStyles((theme) => ({
+  root: {
+    alignItems: 'center',
+    backgroundColor: theme.palette.grey[100],
+    borderBottom: `1px solid ${theme.palette.divider}`,
+    display: 'flex',
+    padding: theme.spacing(2),
+    position: 'relative',
+  },
+
+  closable: {
+    padding: theme.spacing(1, 2),
+  },
+
+  title: {
+    flex: 1,
+  },
+}));
+
+function DialogTitle(props) {
+  const classes = styles();
+
+  const { closable, onCloseButtonClick, title } = props;
+
+  const handleCloseButtonClick = useCallback((e) => {
+    onCloseButtonClick(e);
+  }, [onCloseButtonClick]);
+
+  return (
+    <MUIDialogTitle
+      className={clsx(
+        classes.root,
+        closable ? classes.closable : null,
+      )}
+      disableTypography
+    >
+      <Typography
+        className={classes.title}
+        variant="h2"
+      >
+        {title}
+      </Typography>
+
+      {closable && (
+        <IconButton
+          edge="end"
+          onClick={handleCloseButtonClick}
+        >
+          <CloseIcon />
+        </IconButton>
+      )}
+    </MUIDialogTitle>
+  );
+}
+
+DialogTitle.propTypes = {
+  closable: PropTypes.bool,
+  onCloseButtonClick: PropTypes.func,
+  title: PropTypes.string.isRequired,
+};
+
+DialogTitle.defaultProps = {
+  closable: true,
+};
+
+export default DialogTitle;
