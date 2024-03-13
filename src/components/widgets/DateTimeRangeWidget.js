c4b42f95a673fbb1198c0460bd929e6c936efa55
Allan Hart <allan.w.hart@gmail.com>
2024-03-12 21:21:21 -0600

    Roughed in DateTimeRangeWidget

--- src/components/widgets/DateTimeRangeWidget.js
+++ src/components/widgets/DateTimeRangeWidget.js
@@ -4,62 +4,83 @@
 *
 */
 
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import FormControl from '@material-ui/core/FormControl';
-import IconButton from '@material-ui/core/IconButton';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import { withStyles } from '@material-ui/core/styles';
-import CloseIcon from '@material-ui/icons/Close';
+// import Button from '@material-ui/core/Button';
+// import FormControl from '@material-ui/core/FormControl';
+// import IconButton from '@material-ui/core/IconButton';
+// import Input from '@material-ui/core/Input';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import { withStyles } from '@material-ui/core/styles';
+// import CloseIcon from '@material-ui/icons/Close';
 
 import DateWidget from './DateWidget';
 
 
 
 
-class DateTimeRangeWidget extends React.PureComponent {
-  // static toRepresentation(value) {
-  //   return value ? value.url : null;
-  // }
+function DateTimeRangeWidget(props) {
+  const {
+    error,
+    fieldInfo,
+    label,
+    onChange,
+    value,
+  } = props;
 
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      selectedModel: props.value,
-      buttonLabel: ''
-    };
-  }
+  const [dateRange, setDateRange] = useState(value);
 
-  render() {
-    const {
-      classes,
-      fieldInfo,
-      label,
-      value,
-    } = this.props;
 
-    return (
-      <Box component="fieldset" display="flex" flexDirection="column" gridGap={16}>
-        <DateWidget
-          fieldInfo={{ type: 'date_and_time' }}
-          label="Start"
-        />
-        <DateWidget
-          fieldInfo={{ type: 'date_and_time' }}
-          label="End"
-        />
-      </Box>
-    );
-  }
+  const handleDateChange = (property) => (value) => {
+    const _dateRange = { ...dateRange, [property]: value };
+    setDateRange(_dateRange);
+    onChange(_dateRange);
+  };
+
+
+  return (
+    <Box component="fieldset" display="flex" flexDirection="column" gridGap={16}>
+      <DateWidget
+        fieldInfo={{ type: 'date_and_time' }}
+        label="Start"
+        onChange={handleDateChange('lower')}
+        value={dateRange.start}
+      />
+      <DateWidget
+        fieldInfo={{ type: 'date_and_time' }}
+        label="End"
+        onChange={handleDateChange('upper')}
+        value={dateRange.end}
+      />
+    </Box>
+  );
 }
 
+
+DateTimeRangeWidget.fromRepresentation = (value, fieldInfo) => {
+  const repr = {
+    lower: null,
+    upper: null,
+  };
+
+  if (!value) {
+    return repr;
+  }
+};
+
+
+DateTimeRangeWidget.toRepresentation = (value, fieldInfo) => {
+  return {
+    lower: value.lower ? value.lower.toISOString() : null,
+    upper: value.upper ? value.upper.toISOString() : null,
+  }
+};
+
+
 DateTimeRangeWidget.propTypes = {
-  classes: PropTypes.object.isRequired,
   label: PropTypes.string,
   titleKey: PropTypes.oneOfType([
     PropTypes.func,
@@ -68,16 +89,5 @@ DateTimeRangeWidget.propTypes = {
   value: PropTypes.any,
 };
 
-DateTimeRangeWidget.defaultProps = {
 
-};
-
-export default withStyles((theme) => ({
-  splitStack: theme.mixins.layout.splitStack,
-  button: {
-    minWidth: 'initial',
-  },
-  clearButton: {
-    padding: 6,
-  }
-}))(DateTimeRangeWidget);
+export default DateTimeRangeWidget;
