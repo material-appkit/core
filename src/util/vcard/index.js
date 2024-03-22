0f2cb98153a1262f517f2aadddac5213c1f43a24
Allan Hart <allan.w.hart@gmail.com>
2024-03-22 15:45:45 -0600

    Added vcard parser

---
+++ src/util/vcard/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "parse", {
+  enumerable: true,
+  get: function get() {
+    return _vcard.parse;
+  },
+});
+
+const _vcard = _interopRequireDefault(require("./vcard"));
